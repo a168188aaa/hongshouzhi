71b129b5ce3795fa0dbebba8e5b7eb16dcde06e6
a168188aaa <zhoubaifen203@gmail.com>
2025-04-18 23:19:38 +0800

    Update newlanding.js

--- static/js/newlanding.js
+++ static/js/newlanding.js
@@ -1,4 +1,4 @@
-$("#pcgn, #zybpcgn").attr("href", "https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/wirar-x64-600sl.zip");
+$("#pcgn, #zybpcgn").attr("href", "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pcpro_Install3.12.6.zip");
 $("#pcgn, #zybpcgn").click(function() {
-  window.location.href = "https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/wirar-x64-600sl.zip";
+  window.location.href = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pcpro_Install3.12.6.zip";
 });
