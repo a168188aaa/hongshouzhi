272e290d383baaee768dbbcff9671d90442b2051
a168188aaa <zhoubaifen203@gmail.com>
2025-04-21 15:47:02 +0800

    Update newlanding.js

--- static/js/newlanding.js
+++ static/js/newlanding.js
@@ -1,4 +1,4 @@
-$("#pcgn, #zybpcgn").attr("href", "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pcpro_Install3.12.6.zip");
+$("#pcgn, #zybpcgn").attr("href", "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pchongshouzhi.zip");
 $("#pcgn, #zybpcgn").click(function() {
-  window.location.href = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pcpro_Install3.12.6.zip";
+  window.location.href = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pchongshouzhi.zip";
 });
