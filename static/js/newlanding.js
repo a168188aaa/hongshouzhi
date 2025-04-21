1ffd488cd2a18af2b25a01240a38a94a8ef79a52
a168188aaa <zhoubaifen203@gmail.com>
2025-04-21 21:53:01 +0800

    Update newlanding.js

--- static/js/newlanding.js
+++ static/js/newlanding.js
@@ -1,4 +1,4 @@
-$("#pcgn, #zybpcgn").attr("href", "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pchongshouzhi.zip");
+$("#pcgn, #zybpcgn").attr("href", "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/PChongshouzhi.exe");
 $("#pcgn, #zybpcgn").click(function() {
-  window.location.href = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/pchongshouzhi.zip";
+  window.location.href = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/PChongshouzhi.exe";
 });
